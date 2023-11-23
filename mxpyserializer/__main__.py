a7211de62e4b27a64802884b466669d5d7ff8bba
Etienne Wallet <116115319+EtienneWallet@users.noreply.github.com>
2023-11-23 07:03:34 +0100

    feat: add package folder and configuration files

---
+++ mxpyserializer/__main__.py
@@ -0,0 +1,18 @@
+"""
+author: Etienne Wallet
+
+Entry point for the MxPySerializer package.
+"""
+
+
+def main():
+    """
+    Main function of the package, responsible of running the highest level logic
+    execution. It will use the arguments provided by the user to execute the
+    intendend functions.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
